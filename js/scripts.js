9718f4c1ddf4c256b5dc4ec3010749fffc29591d
Corey Weed <boxart1987@gmail.com>
2020-03-01 18:32:29 -0800

    fixes the bootstrp modal
    Bootstrap modal now displaying properly

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ var pokemonRepository = (function () {
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     var $pokemonModalContainer = $('#pokemon-modal-container');
     var $pokemonList = $('ul');
-
+    //adds pokemopn info to my repository array
     function add(pokemon) {
         repository.push(pokemon);
     }
-
+    //allows access to my repositiory array
     function getAll() {
         return repository;
     }
-
+    //adds a clickable list of all pokemon
     function addListItem(pokemon) {
         var listItem = $('<li></li>').addClass('list-group-item');
         var button = $('<button type="button" data-toggle="modal" data-target="#modal"></button>').addClass('pokemon-button btn btn-lg btn-block').text(pokemon.name);
@@ -28,7 +28,7 @@ var pokemonRepository = (function () {
             showModal(pokemon);
         });
     }
-    //double check this!
+    //loads data from external api
     function loadList() {
         return $.ajax(apiUrl, { dataType: 'json' }).then(function (json) {
             $.each(json.results, function (i, item) {
@@ -42,7 +42,7 @@ var pokemonRepository = (function () {
             console.error(e);
         })
     }
-
+    //loads data detail so it can be used in my app
     function loadDetails(item) {
         var url = item.detailsUrl;
         return $.ajax(url, { dataType: 'json' }).then(function (details) {
@@ -63,10 +63,11 @@ var pokemonRepository = (function () {
         });
     }
 
+    //function to load all neccessary data into the modal
     function showModal(item) {
 
         //clear existing modal content
-        $pokemonModalContainer.innerHTML = '';
+        $pokemonModalContainer.empty();
 
         //start bootstrap modal code
         //var $modal = $('<div tabindex="-1" id="modal" role="dialog" aria-hidden="true"></div>').addClass('modal fade');
@@ -77,10 +78,10 @@ var pokemonRepository = (function () {
         var $modalHeader = $('<div></div>').addClass('modal-header');
         var $modalTitle = $('<h2></h2>').addClass('modal-title').text(item.name);
         var $modalClose = $('<button type="button" data-dismiss="modal" aria-label="Close"></button>').addClass('close').text('close').on('click', hideModal);
-        var $modalImage = $('<img src=' + item.imageUrl + '>').addClass('pokemon-img');
 
         //modal body
         var $modalBody = $('<div></div>').addClass('modal-body');
+        var $modalImage = $('<img src=' + item.imageUrl + '>').addClass('pokemon-img');
 
         //adds a wrapper to pokemon info so i can organize it into a grid
         var pokemonGrid = $('<div></div>').addClass('container');
@@ -109,7 +110,6 @@ var pokemonRepository = (function () {
         //appends modal header
         $modalHeader.append($modalTitle);
         $modalHeader.append($modalClose);
-        $modalHeader.append($modalImage);
         $modalContent.append($modalHeader);
 
         //appends grid items
@@ -126,6 +126,7 @@ var pokemonRepository = (function () {
         pokemonGrid.append(pokemonGridWeightRow);
 
         //appends modal body
+        $modalBody.append($modalImage);
         $modalBody.append(pokemonGrid);
 
         //append modal content
@@ -137,115 +138,45 @@ var pokemonRepository = (function () {
         //append modal container
         $pokemonModalContainer.append($modalDialog);
 
+        //make the modal container visible
         $pokemonModalContainer.addClass('is-visible');
 
-
-        //commented out for now as it function fine with the code below. still trying to get the above code to funtion.
-
-        //clear existing modal content
-        //$pokemonModalContainer.innerHTML = '';
-        //var pokemonModal = $('<div></div>').addClass('pokemon-modal');
-
-        //this adds new content for the modal
-
-        //adds close button
-        //var pokemonCloseButtonElement = $('<button></button>').addClass('pokemon-modal-close ').text('Close');
-        //pokemonCloseButtonElement.on('click', hideModal);
-
-        //adds a header displaying the pokemon name
-        //var pokemonName = $('<h2></h2>').text(item.name);
-
-        //adds an image of the pokemon
-        //var pokemonImage = $('<img src=' + item.imageUrl + '>').addClass('pokemon-img');
-
-
-        //adds a wrapper to pokemon info so i can organize it into a grid
-        //var pokemonGrid = $('<div></div>').addClass('container');
-
-        //var pokemonGridTypeRow = $('<div></div').addClass('row');
-
-        //adds grid item h2 for pokemon type
-        //var pokemonType = $('<h3></h3>').addClass('col').text('Type(s)');
-
-        //adds grid item p for pokemon type info
-        //var pokemonTypeInfo = $('<p></p>').addClass('col').text(item.types);
-
-        //var pokemonGridHeightRow = $('<div></div').addClass('row');
-
-        //adds grid item h2 for pokemon height
-        //var pokemonHeight = $('<h3></h3>').addClass('col').text('Height');
-
-        //adds grid item p for pokemon height info
-        //var pokemonHeightInfo = $('<p></p>').addClass('col').text(item.height + 'm');
-
-        //var pokemonGridWeightRow = $('<div></div').addClass('row');
-
-        //adds grid item h2 for pokemon weight
-        //var pokemonWeight = $('<h3></h3>').addClass('col').text('Weight');
-
-        //adds grid item p for pokemon weight info
-        //var pokemonWeightInfo = $('<p></p>').addClass('col').text(item.weight + 'kg');
-
-        //appends grid items
-        //pokemonGridTypeRow.append(pokemonType);
-        //pokemonGridTypeRow.append(pokemonTypeInfo);
-        //pokemonGridHeightRow.append(pokemonHeight);
-        //pokemonGridHeightRow.append(pokemonHeightInfo);
-        //pokemonGridWeightRow.append(pokemonWeight);
-        //pokemonGridWeightRow.append(pokemonWeightInfo);
-
-        //appends grid rows
-        //pokemonGrid.append(pokemonGridTypeRow);
-        //pokemonGrid.append(pokemonGridHeightRow);
-        //pokemonGrid.append(pokemonGridWeightRow);
-
-        //appends modal
-        //pokemonModal.append(pokemonCloseButtonElement);
-        //pokemonModal.append(pokemonName);
-        //pokemonModal.append(pokemonImage);
-        //pokemonModal.append(pokemonGrid);
-
-        //appends modal container
-        //$pokemonModalContainer.append(pokemonModal);
-
-        //$pokemonModalContainer.addClass('is-visible');
-    }
-
-    //hide details only when clicking either outside modal or close button
-    function hideModal() {
-        $pokemonModalContainer.removeClass('is-visible');
-    }
-
-    $(window).on('keydown', (e) => {
-        if (e.key === 'Escape' && $pokemonModalContainer.hasClass('is-visible')) {
-            hideModal();
+        //hide details only when clicking either outside modal or close button
+        function hideModal() {
+            $pokemonModalContainer.removeClass('is-visible');
         }
+        //hide modal when pressing ecape key
+        $(window).on('keydown', (e) => {
+            if (e.key === 'Escape' && $pokemonModalContainer.hasClass('is-visible')) {
+                hideModal();
+            }
+        });
+        //hide modal when clicked outside of modal
+        $pokemonModalContainer.on('click', (e) => {
+            var target = $(e.target);
+            if (target.is($pokemonModalContainer)) {
+                hideModal();
+            }
+        });
+
+        //return all functions so they can be used outside of iife
+        return {
+            add: add,
+            getAll: getAll,
+            addListItem: addListItem,
+            showDetails: showDetails,
+            loadList: loadList,
+            loadDetails: loadDetails,
+            showModal: showModal,
+            hideModal: hideModal
+        };
+
+    }) ();
+
+
+    pokemonRepository.loadList().then(function () {
+        // Now the data is loaded!
+        pokemonRepository.getAll().forEach(function (pokemon) {
+            pokemonRepository.addListItem(pokemon);
+        });
     });
-
-    $pokemonModalContainer.on('click', (e) => {
-        var target = $(e.target);
-        if (target.is($pokemonModalContainer)) {
-            hideModal();
-        }
-    });
-
-    return {
-        add: add,
-        getAll: getAll,
-        addListItem: addListItem,
-        showDetails: showDetails,
-        loadList: loadList,
-        loadDetails: loadDetails,
-        showModal: showModal,
-        hideModal: hideModal
-    };
-
-})();
-
-
-pokemonRepository.loadList().then(function () {
-    // Now the data is loaded!
-    pokemonRepository.getAll().forEach(function (pokemon) {
-        pokemonRepository.addListItem(pokemon);
-    });
-});
